2064a12db7220d0a7270a19ac606f49aef063c25
RealCaliforniaCheese <chechaohsu@che-chao-hsus-macbook-pro.local>
2015-12-31 19:15:27 -0500

    Pre-work submission
    Fixed dynamic slider
    Added snap to whole numbers
    Added more state variables
    Added UI

--- Tipster/SettingsViewController.swift
+++ Tipster/SettingsViewController.swift
@@ -23,126 +23,97 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var tipDefaultControl: UISegmentedControl!
     
-    var tipDefaultTitle: Float?
-    var tipPercentages = [Float](count: 3, repeatedValue: 0.00)
-    
     let defaults = NSUserDefaults.standardUserDefaults()
     
     // These number values represent each slider position
-    var numbers = [1...50]
-    var oldIndex = 0
-    
-    
-    /*func bubbleSort(tipUnsortedPercentage: Float?) {
-        for var i = 0; i <= 2; i++ {
-            if tipUnsortedPercentage <= tipPercentages[i] {
-                if i != 0 { // i=1
-                    tipPercentages[i-1] = tipPercentages[i]
-
-                }
-                tipPercentages[i] = tipUnsortedPercentage!
-            }
-            // Replace last (highest) tip if tipUnsortedPercentage is greater than all
-            else if tipUnsortedPercentage > tipPercentages[2] {
-                tipPercentages[0] = tipPercentages[1]
-                tipPercentages[1] = tipPercentages[2]
-                tipPercentages[2] = tipUnsortedPercentage!
-                
-            }
-        }
-    }
-    
-    func setTipDefault() {
-        for var i = 0; i <= 2; i++ {
-            tipDefaultControl.setTitle("\(tipPercentages[i])", forSegmentAtIndex: i)
-        }
-    }*/
+    var stepValue:Float32 = 1.00
+    var newStep:Float32 = 0.00
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tipLowMinLabel.text  = "0.00"
-        tipMidMinLabel.text  = "0.00"
-        tipHighMinLabel.text = "0.00"
-        tipLowMaxLabel.text  = "50.00"
-        tipMidMaxLabel.text  = "50.00"
-        tipHighMaxLabel.text = "50.00"
-        tipLowSlider.value = defaults.floatForKey("tipLow")
-        tipMidSlider.value = defaults.floatForKey("tipMid")
-        tipHighSlider.value = defaults.floatForKey("tipHigh")
-        tipDefaultControl.setTitle("\(tipLowSlider.value)", forSegmentAtIndex: 0)
-        tipDefaultControl.setTitle("\(tipMidSlider.value)", forSegmentAtIndex: 1)
-        tipDefaultControl.setTitle("\(tipHighSlider.value)", forSegmentAtIndex: 2)
-
-        // Do any additional setup after loading the view.
-        tipLowSlider.value = 15.0
-        // tipLowSlider = UISlider(frame: self.view.bounds)
-        // self.view.addSubview(tipLowSlider!) // ERROR here
-            
-        // slider values go from 0 to the number of values in your numbers array
-        // let numberOfSteps = Float(numbers.count - 1)
-        // tipLowSlider!.minimumValue = 10;
-        // tipLowSlider!.maximumValue = numberOfSteps;
+        tipLowMinLabel.text  = "10.0"
+        tipMidMinLabel.text  = "15.0"
+        tipHighMinLabel.text = "20.0"
+        tipLowMaxLabel.text  = "20.0"
+        tipMidMaxLabel.text  = "25.0"
+        tipHighMaxLabel.text = "30.0"
         
-        
-        
-        // If true, valueChanged continously called as the slider moves. If false only when let go.
         tipLowSlider!.continuous = false
         tipMidSlider!.continuous = false
         tipHighSlider!.continuous = false
-        // tipLowSlider!.addTarget(self, action: "tipLowSliderValueChanged:", forControlEvents: .ValueChanged)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tipLowSlider.value = 15.0
+        tipMidSlider.value = 20.0
+        tipHighSlider.value = 25.0
     }
     
-    // MARK: - Navigation
+    
+    override func viewDidAppear(animated: Bool) {
+        tipLowSlider.value   = defaults.floatForKey("tipLow")
+        tipMidSlider.value   = defaults.floatForKey("tipMid")
+        tipHighSlider.value  = defaults.floatForKey("tipHigh")
+        tipLowMinLabel.text  = "10.0"
+        tipMidMinLabel.text  = "\(tipLowSlider.value+1)"
+        tipHighMinLabel.text = "\(tipMidSlider.value+1)"
+        tipLowMaxLabel.text  = "\(tipMidSlider.value-1)"
+        tipMidMaxLabel.text  = "\(tipHighSlider.value-1)"
+        tipHighMaxLabel.text = "30.0"
+        tipMidSlider.minimumValue = tipLowSlider.value+1
+        tipHighSlider.minimumValue = tipMidSlider.value+1
+        tipLowSlider.maximumValue  = tipMidSlider.value-1
+        tipMidSlider.maximumValue  = tipHighSlider.value-1
+        tipDefaultControl.setTitle("\(tipLowSlider.value)", forSegmentAtIndex: 0)
+        tipDefaultControl.setTitle("\(tipMidSlider.value)", forSegmentAtIndex: 1)
+        tipDefaultControl.setTitle("\(tipHighSlider.value)", forSegmentAtIndex: 2)
+        tipDefaultControl.selectedSegmentIndex = defaults.integerForKey("tipDefault")
+
+    }
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
         defaults.synchronize()
     }
+    
     @IBAction func tipLowSliderValueChanged(sender: AnyObject) {
+        // Round to nearest whole integer
+        newStep = roundf((tipLowSlider.value) / stepValue)
+        tipLowSlider.value = newStep * stepValue
+        
         defaults.setFloat(tipLowSlider.value, forKey: "tipLow")
-        if tipLowSlider.value > tipMidSlider.value {
-            tipMidSlider.minimumValue = tipLowSlider.value
-            tipMidMinLabel.text   = "\(tipLowSlider.value)"
-            tipMidMinLabel.text   = String(format: "%.2f", tipLowSlider.value)
-        }
-        if tipLowSlider.value > tipHighSlider.value {
-            tipHighSlider.minimumValue = tipLowSlider.value
-            tipHighMinLabel.text  = "\(tipLowSlider.value)"
-            tipHighMinLabel.text  = String(format: "%.2f", tipLowSlider.value)
+        tipMidSlider.minimumValue = tipLowSlider.value+1
+        tipMidMinLabel.text = "\(tipLowSlider.value+1)"
+        tipMidMinLabel.text = String(format: "%.1f", tipLowSlider.value+1)
+        if tipLowSlider.maximumValue >= tipMidSlider.minimumValue {
+            
         }
         tipDefaultControl.setTitle("\(tipLowSlider.value)", forSegmentAtIndex: 0)
-        // round the slider position to the nearest index of the numbers array
-        /* let index = (Int)(tipLowSlider!.value + 0.5);
-        tipLowSlider?.setValue(Float(index), animated: false)
-        let number = numbers[index]; // <-- This numeric value you want
-        if oldIndex != index {
-            print("sliderIndex:\(index)")
-            print("number: \(number)")
-            oldIndex = index
-        } */
     }
     
     @IBAction func tipMidSliderValueChanged(sender: AnyObject) {
-        if tipMidSlider.value > tipHighSlider.value {
-            tipHighSlider.minimumValue = tipMidSlider.value
-            tipHighMinLabel.text  = "\(tipMidSlider.value)"
-            tipHighMinLabel.text  = String(format: "%.2f", tipLowSlider.value)
-        }
-        tipDefaultControl.setTitle("\(tipMidSlider.value)", forSegmentAtIndex: 1)
+        newStep = roundf((tipMidSlider.value) / stepValue)
+        tipMidSlider.value = newStep * stepValue
+        
         defaults.setFloat(tipMidSlider.value, forKey: "tipMid")
+        tipLowSlider.maximumValue = tipMidSlider.value-1
+        tipLowMaxLabel.text  = "\(tipMidSlider.value-1)"
+        tipLowMaxLabel.text  = String(format: "%.1f", tipMidSlider.value-1)
+        tipHighSlider.minimumValue = tipMidSlider.value + 1
+        tipHighMinLabel.text  = "\(tipMidSlider.value+1)"
+        tipHighMinLabel.text  = String(format: "%.1f", tipMidSlider.value+1)
+        tipDefaultControl.setTitle("\(tipMidSlider.value)", forSegmentAtIndex: 1)
     }
     
     @IBAction func tipHighSliderValueChanged(sender: AnyObject) {
+        newStep = roundf((tipHighSlider.value) / stepValue)
+        tipHighSlider.value = newStep * stepValue
+        
         defaults.setFloat(tipHighSlider.value, forKey: "tipHigh")
-        // bubbleSort(tipHighSlider.value)
-        // setTipDefault()
+        tipMidSlider.maximumValue = tipHighSlider.value-1
+        tipMidMaxLabel.text  = "\(tipHighSlider.value-1)"
+        tipMidMaxLabel.text  = String(format: "%.1f", tipHighSlider.value-1)
         tipDefaultControl.setTitle("\(tipHighSlider.value)", forSegmentAtIndex: 2)
+    }
+    
+    @IBAction func tipDefaultValueChanged(sender: AnyObject) {
+        defaults.setInteger(tipDefaultControl.selectedSegmentIndex, forKey: "tipDefault")
     }
 }
--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -10,51 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField:  UITextField!
+    @IBOutlet weak var tipLabel:   UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tabStepper: UIStepper!
+    @IBOutlet weak var tabQtyField: UITextField!
+    @IBOutlet weak var tabLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
     
     let defaults = NSUserDefaults.standardUserDefaults()
-    var tipLowSetting, tipMidSetting, tipHighSetting: Float?
+    var tipLowSetting:Float32   = 15.0
+    var tipMidSetting:Float32   = 20.0
+    var tipHighSetting:Float32  = 25.0
+    var billAmount:Float32      = 0.00
+    var tip:Float32             = 0.00
+    var total:Float32           = 0.00
+    var tab:Float32             = 0.00
+    var tipPercentages          = [Float](count: 3, repeatedValue: 0.0)
+    var tipPercentage:Float32   = 0.00
+    
+    let imgArray                = ["low", "mid", "high"]
+    
+    func setLabelTotals () {
+        tipPercentages = [tipLowSetting, tipMidSetting, tipHighSetting]
+        tipPercentage  = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        billAmount = NSString(string: billField.text!).floatValue
+        tip = billAmount * (tipPercentage/100)
+        total = billAmount + tip
+        tab = total / (Float32)(tabStepper.value)
+        
+        tipLabel.text    = "$\(tip)"
+        totalLabel.text  = "$\(total)"
+        tabLabel.text    = "$\(tab)"
+        tabQtyField.text = "\((Int)(tabStepper.value))"
+        tipLabel.text    = String(format: "$%.2f", tip)
+        totalLabel.text  = String(format: "$%.2f", total)
+        tabLabel.text    = String(format: "$%.2f", tab)
+    }
+    
+    func setTipDefaultControl () {
+        tipLowSetting  = defaults.floatForKey("tipLow")
+        tipMidSetting  = defaults.floatForKey("tipMid")
+        tipHighSetting = defaults.floatForKey("tipHigh")
+        
+        tipControl.setTitle("\(tipLowSetting)", forSegmentAtIndex: 0)
+        tipControl.setTitle("\(tipMidSetting)", forSegmentAtIndex: 1)
+        tipControl.setTitle("\(tipHighSetting)", forSegmentAtIndex: 2)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text   = "$0.00"
-        totalLabel.text = "$0.00"
+        tipLabel.text    = "$0.00"
+        totalLabel.text  = "$0.00"
+        tabQtyField.text = "\((Int)(tabStepper.value))"
+        tabLabel.text    = "$0.00"
+        
+        defaults.setFloat(tipLowSetting, forKey: "tipLow")
+        defaults.setFloat(tipMidSetting, forKey: "tipMid")
+        defaults.setFloat(tipHighSetting, forKey: "tipHigh")
+        
+        setTipDefaultControl()
     }
     
     override func viewDidAppear(animated: Bool) {
-        tipLowSetting = defaults.floatForKey("tipLow")
-        tipMidSetting = defaults.floatForKey("tipMid")
-        tipHighSetting = defaults.floatForKey("tipHigh")
+        tipControl.selectedSegmentIndex = defaults.integerForKey("tipDefault")
         
-        tipControl.setTitle("\(tipLowSetting!)", forSegmentAtIndex: 0)
-        tipControl.setTitle("\(tipMidSetting!)", forSegmentAtIndex: 1)
-        tipControl.setTitle("\(tipHighSetting!)", forSegmentAtIndex: 2)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setLabelTotals()
+        setTipDefaultControl()
+        billField.becomeFirstResponder()
+        imgView.image = UIImage(named: imgArray[tipControl.selectedSegmentIndex])
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        let tipPercentages = [tipLowSetting, tipMidSetting, tipHighSetting]
-        let tipPercentage  = tipPercentages[tipControl.selectedSegmentIndex]
-    
-        let billAmount = NSString(string: billField.text!).floatValue
-        let tip = billAmount * tipPercentage! / 100
-        let total = (billAmount + tip)
-        
-        tipLabel.text   = "$\(tip)"
-        totalLabel.text = "$\(total)"
-        tipLabel.text   = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        setLabelTotals()
     }
 
+    @IBAction func onSegmentedContChanged(sender: AnyObject) {
+        imgView.image = UIImage(named: imgArray[tipControl.selectedSegmentIndex])
+    }
+    
+    @IBAction func onTabStepChanged(sender: AnyObject) {
+        setLabelTotals()
+    }
+    
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
